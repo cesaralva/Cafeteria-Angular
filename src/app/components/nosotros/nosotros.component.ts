345a48075a93348e4f22de6a12bc8c8d68603b89
CesarAlva <66078322+cesaralva@users.noreply.github.com>
2024-05-09 14:13:47 -0500

    nosotros torminado

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LacartaComponent } from '../lacarta/lacarta.component';
-
+import 'animate.css';
 @Component({
   selector: 'app-nosotros',
   standalone: true,
