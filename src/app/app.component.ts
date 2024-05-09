67c3cf5c8fdd25a89eea917bf0f837f56e3978e3
developer <developer@Developer.developer>
2024-05-08 23:17:05 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet,NosotrosComponent,NavbarComponent]
 })
 export class AppComponent {
   title = 'Proyecto-Cafe';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'nosotros', component: NosotrosComponent}];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
