e301399cc0b055603a96ece686986515f9fa6cf3
CesarAlva <66078322+cesaralva@users.noreply.github.com>
2024-05-10 12:41:52 -0500

    home concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LacartaComponent } from './components/lacarta/lacarta.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet,NosotrosComponent,NavbarComponent,HomeComponent]
+    imports: [RouterOutlet,NosotrosComponent,NavbarComponent,HomeComponent,NosotrosComponent,RouterOutlet,RouterLink,LacartaComponent,HomeComponent,ReservasComponent]
 })
 export class AppComponent {
   title = 'Proyecto-Cafe';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, RouterOutlet,RouterLink,HomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
